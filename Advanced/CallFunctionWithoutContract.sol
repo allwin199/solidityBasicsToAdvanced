07ab788172939fde2b92fa964bc971a9a7c4b974
worksofallwin <princeallwin199@gmail.com>
2024-01-17 16:51:20 +0530

    complete callFunctionWithoutContract

---
+++ Advanced/CallFunctionWithoutContract.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {CallAnything} from "./CallAnything.sol";
+
+contract CallFunctionWithoutContract {
+    CallAnything callAnything;
+
+    constructor(address callAnythingContract) {
+        callAnything = CallAnything(callAnythingContract);
+    }
+
+    // with a staticcall, we can have this be a view function!
+    function staticCallFunctionDirectly() public view returns (bytes4, bool) {
+        (bool success, bytes memory returnData) =
+            address(callAnything).staticcall(abi.encodeWithSignature("getFunctionSelector()"));
+        return (bytes4(returnData), success);
+    }
+
+    // by calling this fn, we are updating storage in callAnything contract
+    function callTransferFunctionDirectly(address someAddress, uint256 amount) public returns (bytes4, bool) {
+        (bool success, bytes memory returnData) =
+            address(callAnything).call(abi.encodeWithSignature("transfer(address,uint256)", someAddress, amount));
+        return (bytes4(returnData), success);
+    }
+}
