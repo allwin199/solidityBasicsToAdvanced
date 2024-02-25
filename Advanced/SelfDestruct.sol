ab0cc6f0d43e6741d74def04ab6f6d4716fed7f3
Prince Allwin <princeallwin199@gmail.com>
2024-02-25 11:16:53 +0530

    Update SelfDestruct.sol

--- Advanced/SelfDestruct.sol
+++ Advanced/SelfDestruct.sol
@@ -67,3 +67,33 @@ contract Attack {
         selfdestruct(addr);
     }
 }
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract EtherGame {
+    uint public targetAmount = 3 ether;
+    uint public balance;
+    address public winner;
+
+    function deposit() public payable {
+        require(msg.value == 1 ether, "You can only send 1 Ether");
+
+        // instead of using `balance == address(this).balance`
+        // We are keeping track of the balances in the `balance` variable.
+        // Now if the attacker, sends eth without calling the `deposit` `balance` will not increase.
+        balance += msg.value;
+        require(balance <= targetAmount, "Game is over");
+
+        if (balance == targetAmount) {
+            winner = msg.sender;
+        }
+    }
+
+    function claimReward() public {
+        require(msg.sender == winner, "Not winner");
+
+        (bool sent, ) = msg.sender.call{value: balance}("");
+        require(sent, "Failed to send Ether");
+    }
+}
