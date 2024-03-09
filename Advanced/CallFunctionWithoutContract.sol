b3143813a877cc4eeb3969ce00ee91a300931c41
Prince Allwin <princeallwin199@gmail.com>
2024-03-09 13:10:03 +0530

    Update CallFunctionWithoutContract.sol

--- Advanced/CallFunctionWithoutContract.sol
+++ Advanced/CallFunctionWithoutContract.sol
@@ -17,6 +17,10 @@ contract CallFunctionWithoutContract {
         return (bytes4(returnData), success);
     }
 
+    // since we have the contract address of `callAnything` contract
+    // and we know which function to call `transfer`
+    // we can encode the `transfer` function with `encodeWithSignature` and call directly
+    // by calling this fn, we are updating storage in `callAnything` contract
     // by calling this fn, we are updating storage in callAnything contract
     function callTransferFunctionDirectly(address someAddress, uint256 amount) public returns (bytes4, bool) {
         (bool success, bytes memory returnData) =
