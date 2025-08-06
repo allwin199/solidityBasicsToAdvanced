142d19106d3d122c4707161b79abd82f84c0df28
Prince Allwin <princeallwin199@gmail.com>
2025-08-06 19:01:25 +0530

    Update 01-Types.sol

--- Basics/01-Types.sol
+++ Basics/01-Types.sol
@@ -25,9 +25,19 @@ contract SimpleStorage {
     address myAddress = 0x316e011b77f3cf6196ce4e7a87779cf0B03BD986;
 
     // bytes
-    bytes8 favouriteBytes = "cat";
+    bytes32 favouriteBytes = "cat";
     // strings are secretly bytes object but only for text.
     // cat will be automatically converted into some bytes object 0x12345asas
+
+    // Bytes are a collection of characters written in hexadecimal representation.
+
+    bytes1 minBytes = "I am a fixed size byte array of 1 byte";
+    bytes32 maxBytes = "I am a fixed size byte array of 32 bytes";
+    bytes dynamicBytes = "I am a dynamic array, so you can manipulate my size";
+
+    // Bytes can be allocated in size (up to bytes32). However, bytes and bytes32 represent distinct data types.
+
+    // Strings are internally represented as dynamic byte arrays (bytes type) and designed specifically for working with text. For this reason, a string can easily be converted into bytes.
 }
 
 
